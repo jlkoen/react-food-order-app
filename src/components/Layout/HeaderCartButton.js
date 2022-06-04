e2ba323e3c5841c61ddcd0af8341af8fa5c01174
John Koen <jkoen@smartronix.com>
2022-06-04 14:42:02 -0400

    Added initial Cart Context and Reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,15 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/CartContext';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button
       className='flex cursor-default space-x-2 absolute right-0 text-sm justify-around items-center bg-very-peri-700 rounded-3xl font-bold hover:bg-very-peri-900 px-5 py-2'
@@ -10,7 +19,9 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className='bg-very-peri-600 rounded-3xl px-3 py-1 text-xs'>3</span>
+      <span className='bg-very-peri-600 rounded-3xl px-3 py-1 text-xs'>
+        {numberOfCartItems}
+      </span>
     </button>
   );
 };
